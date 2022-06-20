aaab79987d07c5cc3a407e2ba127049b4a673d53
J4F3ET <jslopezz@correo.udistrital.edu.co>
2022-06-20 16:27:45 -0500

    Taller

---
+++ POO/TallerPunto3.11.cpp
@@ -0,0 +1,9 @@
+// (Modifi cación de la clase LibroCalificaciones) Modifi que la clase LibroCalificaciones (fi guras 3.11 a 3.12) de la
+// siguiente manera:
+// a) Incluya un segundo miembro de datos string, que represente el nombre del instructor del curso.
+// b) Proporcione una función establecer para modifi car el nombre del instructor, y una función obtener para obtenerlo.
+// c) Modifi que el constructor para especifi car dos parámetros: uno para el nombre del curso y otro para el nombre del
+// instructor.
+// d) Modifi que la función mostrarMensaje, de tal forma que primero imprima el mensaje de bienvenida y el nombre
+// del curso, y que después imprima "Este curso es presentado por: ", seguido del nombre del instructor.
+// Use su clase modifi cada en un programa de prueba que demuestre las nuevas capacidades de la clase.
